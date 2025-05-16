09123ea22730e1846a46008aab861458d07bbebb
haenin <143497104+haenin@users.noreply.github.com>
2025-05-16 23:48:08 +0900

    <!-- 화면구현 -->

--- app/src/main/java/com/example/project/CameraCaptureActivity.java
+++ app/src/main/java/com/example/project/CameraCaptureActivity.java
@@ -109,4 +109,3 @@ public class CameraCaptureActivity extends AppCompatActivity {
         }
     }
 }
-
--- app/src/main/java/com/example/project/CameraResultActivity.java
+++ app/src/main/java/com/example/project/CameraResultActivity.java
@@ -25,21 +25,29 @@ public class CameraResultActivity extends AppCompatActivity {
         furnitureInfoTextView = findViewById(R.id.furnitureInfoTextView);
         confirmButton = findViewById(R.id.confirmButton);
 
+        String category = getIntent().getStringExtra("selectedCategory");
+        float measuredDistance = getIntent().getFloatExtra("measuredDistance", 0f); // m 단위
         String imagePath = getIntent().getStringExtra("imagePath");
-        String selectedCategory = getIntent().getStringExtra("selectedCategory");
 
         if (imagePath != null) {
             Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
             capturedImageView.setImageBitmap(bitmap);
         }
 
-        // 예: 가구 정보 추정
-        furnitureInfoTextView.setText("가구: " + selectedCategory + "\n폐기물 규격: ?");
+        int price = WastePriceCalculator.calculatePrice(category, measuredDistance);
+
+
+        // UI에 표시
+        furnitureInfoTextView.setText(
+                "가구: " + category + "\n" +
+                        "스티커 가격: " + price + "원"
+        );
 
         confirmButton.setOnClickListener(v -> {
             Intent intent = new Intent(CameraResultActivity.this, StickerPriceActivity.class);
             intent.putExtra("imagePath", imagePath);
-            intent.putExtra("result", selectedCategory);
+            intent.putExtra("result", category);
+            intent.putExtra("price", price);
             startActivity(intent);
         });
     }
